9c048af7cabb5be679906ab07fca432298abf838
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-03-20 14:13:16 -0400

    Started working on the redux reducer

---
+++ src/actions/TodoActions.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const ADD_TODO = 'ADD_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const FETCH_START = 'FETCH_START';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAILED = 'FETCH_FAILED';
+
+export const getTodos = () => {
+
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import reducer from "./reducers";
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
